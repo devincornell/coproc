3a284d2b7070a3b2f8bbb8c0a788f337d45682b7
Devin J. Cornell <devincornell@github.com>
2023-10-27 19:12:11 -0400

    updating monitor example

--- coproc/monitor/statsresult.py
+++ coproc/monitor/statsresult.py
@@ -26,7 +26,7 @@ class StatsResult:
     def num_notes(self):
         return self.notes.shape[0]
     
-    def save_stats_plot(self, filename: str, **kwargs):
+    def get_stats_plot(self, font_size: int = 5):
         self.stats['memory_usage_gb'] = self.stats['memory_usage'] / 1e9
         p = (plotnine.ggplot(self.stats) 
             + plotnine.aes(x='monitor_minutes', y='memory_usage_gb', group='pid') + plotnine.geom_line()
@@ -40,23 +40,14 @@ class StatsResult:
                 + plotnine.geom_label(
                     data = self.notes,
                     mapping = plotnine.aes(x='monitor_minutes', y='memory_usage_gb', label='text'),
-                    #adjust_text = {
-                    #    #'expand_points': (1.2, 1.2),
-                    #    'arrowprops': {
-                    #        'arrowstyle': '-',
-                    #        #'color': 'black',
-                    #        #'lw': 0.5,
-                    #    },
-                    #},
-                    #size=5,
+                    size=font_size,
                 )
-                #+ plotnine.geom_point(
-                #    data=self.notes,
-                #    mapping = plotnine.aes(x='monitor_minutes', y='memory_usage_gb'),
-                #)
             )
+        return p
         
-        p.save(filename, **kwargs)
+    def save_stats_plot(self, filename: str, font_size: int = 5, **kwargs):
+        p = self.get_stats_plot(font_size=font_size)
+        p.save(filename)
         return p
         
     def save_stats_pyplot(self, filename: str):
