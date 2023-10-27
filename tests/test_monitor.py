e60fe0dd918d0c426bd5b92e4a9463eba4d1cf47
Devin J. Cornell <devincornell@github.com>
2023-10-27 07:23:04 -0400

    improved monitor test

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -19,6 +19,7 @@ def test_monitor():
     import os
     os.makedirs('tmp', exist_ok=True)
     
+    # ask the monitor to regularly save the plot
     monitor = coproc.Monitor(
         snapshot_seconds=0.01,
         fig_fname='tmp/test.png',
@@ -27,22 +28,22 @@ def test_monitor():
     
     with monitor as m:
         a = list()
-        for i in tqdm.tqdm(range(int(1e3))):
-            i
+        for i in tqdm.tqdm(range(int(1e2))):
             a.append(i)
-            #if i > 0 and i % 10000 == 0:
-            if i % 10 == 0:
+            
+            if i % 20 == 0:
                 m.add_note(f'Note {i}')
-                print()
-                result = m.get_stats()
-                print(result.notes)
-                    #m.save_stats_plot(f'tmp/test.png', verbose=False)
-                #try:
-                #    result = m.get_stats()
-                #except BrokenPipeError as e:
-                #    exit()
-                #if result.has_results:
-                #    result.save_stats_plot('tmp/test.png', verbose=False)
+                
+                # ask the monitor to save the stats plot
+                m.save_stats_plot(f'tmp/test.png', verbose=False)
+                
+                # move stats to this process then save as image here
+                try:
+                    result = m.get_stats()
+                except BrokenPipeError as e:
+                    exit()
+                if result.has_results:
+                    result.save_stats_plot('tmp/test.png', verbose=False)
         
         result = m.get_stats()
         print(result.num_stats)
